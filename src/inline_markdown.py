15dbc049d65e11c750a0462dc688884ae16a70a2
Nyior <nyior.clement@aun.edu.ng>
2024-06-12 06:46:17 +0100

    Add doctyping to split nodes function

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -6,14 +6,14 @@ from src.textnode import (
 
 # TODO: Extend function to support multiple levels of nesting
 # E.g: This is an *italic and **bold** word*.
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_nodes = []
+def split_nodes_delimiter(old_nodes, delimiter, text_type) -> list:
+    new_nodes: list = []
     for old_node in old_nodes:
         if old_node.text_type != text_type_text:
             new_nodes.append(old_node)
             continue
-        split_nodes = []
-        sections = old_node.text.split(delimiter)
+        split_nodes: list = []
+        sections: list = old_node.text.split(delimiter)
         if len(sections) % 2 == 0:
             raise ValueError("Invalid markdown, formatted section not closed")
         for i in range(len(sections)):
