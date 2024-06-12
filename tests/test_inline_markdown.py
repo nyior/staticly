7070ba909ae6396e5820bae2907123ce4d9f167e
Nyior <nyior.clement@aun.edu.ng>
2024-06-12 08:31:06 +0100

    Add test case for split node image function

--- tests/test_inline_markdown.py
+++ tests/test_inline_markdown.py
@@ -2,18 +2,24 @@
 
 import unittest
 
-from  src.inline_markdown import split_nodes_delimiter
+from  src.inline_markdown import (
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link
+)
 from src.textnode import (
     TextNode,
     text_type_text,
     text_type_bold,
+    text_type_image,
+    text_type_link
 )
 
 
-class TestInlineMarkdown(unittest.TestCase):
+class TestSplitNodeDelimeter(unittest.TestCase):
 
     def test_raises_exception(self) -> None:
-        old_nodes: list = [TextNode("This is text with a **bolded word", "text")]
+        old_nodes: list = [TextNode("This is text with a **bolded word", text_type_text)]
         
         with self.assertRaises(ValueError) as context:
             split_nodes_delimiter(old_nodes, "**", text_type_bold)
@@ -30,6 +36,37 @@ class TestInlineMarkdown(unittest.TestCase):
         ]
 
         self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes, expected_new_nodes)
+
+
+class TestSplitNodeImage(unittest.TestCase):
+
+    # def test_raises_exception(self) -> None:
+    #     old_nodes: list = [TextNode("This is text with an !(https://storage.googleapis.com/)", text_type_text)]
+        
+    #     with self.assertRaises(ValueError) as context:
+    #         split_nodes_image(old_nodes)
+
+    #     self.assertTrue("Invalid markdown, image section not closed" in str(context.exception))
+
+    def test_eq(self) -> None:
+        old_nodes: list = [
+            TextNode(
+                "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+                text_type_text
+            )
+        ]
+        new_nodes: list = split_nodes_image(old_nodes)
+        expected_new_nodes: list = [
+            TextNode("This is text with an ", text_type_text),
+            TextNode("image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode(" and another ", text_type_text),
+            TextNode(
+                "second image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png"
+            ),
+        ]
+
+        self.assertEqual(len(new_nodes), 4)
         self.assertEqual(new_nodes, expected_new_nodes)
 
 
