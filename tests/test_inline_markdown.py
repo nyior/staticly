59dfb9c47e71486ba8508f5eef1c10a4c13e48be
Nyior <nyior.clement@aun.edu.ng>
2024-06-13 21:21:17 +0100

    Add test case for split node link function

--- tests/test_inline_markdown.py
+++ tests/test_inline_markdown.py
@@ -26,7 +26,7 @@ class TestSplitNodeDelimeter(unittest.TestCase):
 
         self.assertTrue("Invalid markdown, formatted section not closed" in str(context.exception))
 
-    def test_eq(self) -> None:
+    def test_image_eq(self) -> None:
         old_nodes: list = [TextNode("This is text with a **bolded** word", "text")]
         new_nodes: list = split_nodes_delimiter(old_nodes, "**", text_type_bold)
         expected_new_nodes: list = [
@@ -55,6 +55,29 @@ class TestSplitNodeImage(unittest.TestCase):
             TextNode(" and another ", text_type_text),
             TextNode(
                 "second image", text_type_image, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png"
+            ),
+        ]
+
+        self.assertEqual(len(new_nodes), 4)
+        self.assertEqual(new_nodes, expected_new_nodes)
+
+
+class TestSplitNodeLink(unittest.TestCase):
+
+    def test_link_eq(self) -> None:
+        old_nodes: list = [
+            TextNode(
+                "This is text with a [link](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another [second link](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+                text_type_text
+            )
+        ]
+        new_nodes: list = split_nodes_link(old_nodes)
+        expected_new_nodes: list = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("link", text_type_link, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+            TextNode(" and another ", text_type_text),
+            TextNode(
+                "second link", text_type_link, "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png"
             ),
         ]
 
