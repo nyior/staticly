8a827b22b819bacccdb93b38b784f630ae333d81
Nyior <nyior.clement@aun.edu.ng>
2024-08-06 06:45:06 +0100

    Add doctyping to functions

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,12 +1,12 @@
 #markdown_blocks.py
 
 
-def markdown_to_blocks(markdown):
-    blocks = markdown.split("\n\n")
-    filtered_blocks = []
+def markdown_to_blocks(markdown: str) -> list:
+    blocks : list = markdown.split("\n\n")
+    filtered_blocks: list = []
     for block in blocks:
         if block == "":
             continue
-        block = block.strip()
+        block : str = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
--- tests/test_markdown_blocks.py
+++ tests/test_markdown_blocks.py
@@ -3,8 +3,8 @@ from src.markdown_blocks import markdown_to_blocks
 
 
 class TestMarkdownToHTML(unittest.TestCase):
-    def test_markdown_to_blocks(self):
-        md = """
+    def test_markdown_to_blocks(self) -> None:
+        md: str = """
         This is **bolded** paragraph
 
         This is another paragraph with *italic* text and `code` here
@@ -13,18 +13,20 @@ class TestMarkdownToHTML(unittest.TestCase):
         * This is a list
         * with items
         """
-        blocks = markdown_to_blocks(md)
+        blocks: list = markdown_to_blocks(md)
+        expected_blocks: list = [
+            "This is **bolded** paragraph",
+            "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+            "* This is a list\n* with items",
+        ]
+        self.assertEqual(len(blocks), len(expected_blocks))
         self.assertEqual(
             blocks,
-            [
-                "This is **bolded** paragraph",
-                "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
-                "* This is a list\n* with items",
-            ],
+            expected_blocks
         )
 
     def test_markdown_to_blocks_newlines(self):
-        md = """
+        md: str = """
         This is **bolded** paragraph
 
 
@@ -36,14 +38,16 @@ class TestMarkdownToHTML(unittest.TestCase):
         * This is a list
         * with items
         """
-        blocks = markdown_to_blocks(md)
+        blocks: list = markdown_to_blocks(md)
+        expected_blocks: list = [
+            "This is **bolded** paragraph",
+            "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+            "* This is a list\n* with items",
+        ]
+        self.assertEqual(len(blocks), len(expected_blocks))
         self.assertEqual(
             blocks,
-            [
-                "This is **bolded** paragraph",
-                "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
-                "* This is a list\n* with items",
-            ],
+            expected_blocks
         )
 
 
