611373d9f8e8cf2843480f2f261484e638a7426c
Nyior <nyior.clement@aun.edu.ng>
2024-08-07 21:54:22 +0100

    Remove console log lines

--- tests/test_markdown_blocks.py
+++ tests/test_markdown_blocks.py
@@ -14,7 +14,6 @@ This is the same paragraph on a new line
 * with items
 """
         blocks: list = markdown_to_blocks(md)
-        print(f"\n{blocks}\n")
         expected_blocks: list = [
             "This is **bolded** paragraph",
             "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
