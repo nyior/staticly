d4842d8fd9892e74ea03b8e7194cfcaec74f8c5c
Nyior <nyior.clement@aun.edu.ng>
2024-08-08 22:21:52 +0100

    Add recursive function for copying static files

--- src/main.py
+++ src/main.py
@@ -1,14 +1,24 @@
 # main.py
+import os
+import shutil
 
-from textnode import TextNode
+from copystatic import copy_files_recursive
 
 
-def main() -> None:
-    textnode = TextNode(
-        text="Sample text node", text_type="bold", url="sample url"
-    )
-    print(textnode)
+dir_path_static = "./static"
+dir_path_public = "./public"
 
+
+def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+
+main()
 
 if __name__ == "__main__":
     main()   
