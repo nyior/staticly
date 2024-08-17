f4ec22dc4f32ad305d7a5a94df3b5bd45ccfc2dd
Nyior <nyior.clement@aun.edu.ng>
2024-08-17 09:30:45 +0100

    update main function

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 # main.py
+
 import os
 import shutil
 
 from copystatic import copy_files_recursive
-
+from gencontent import generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
@@ -17,8 +20,13 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
-main()
 
 if __name__ == "__main__":
     main()   
